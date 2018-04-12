d1b2655e26d64f288999125e45ff4e201ef8504e
duartefalmeida <34278054+duartefalmeida@users.noreply.github.com>
2018-04-12 19:15:45 +0100

    Update RegExp.ml

--- RegExp.ml
+++ RegExp.ml
@@ -64,34 +64,51 @@ let rec string_of_list l =
 
 (* matchAtStart *)
 
+let rec matchAtStartStr line str_in_list =
+	match line, str_in_list with
+		[],_ -> (false, [], [])
+		| _,[] -> (true, [], line)
+		| x::xs,y::ys -> let(b, m, r) = matchAtStartStr xs ys in
+					if x=y && b then (b, y::m, r) 
+					else (false, [], [])
+;;
+
+let rec str_contain_elem str_in_list elem =
+		match str_in_list with
+		| [] -> false
+		| x::xs -> if x = elem then true 
+				else str_contain_elem xs elem
+;;
+
+
 let rec matchAtStartRE line re =
     match re with
-      | Zero p -> (true, [], line)
-      | _ -> (false, [], [])
-      | Any -> (true, List.hd line, List.tl line)
+      	Zero p -> (true, [], line)
+      
+      | Any -> (true, [List.hd line], List.tl line)
       | Str str -> matchAtStartStr line (list_of_string str)
-			| Class str -> match line with
-					[] -> (false, [], [])
-					|x::xs -> if str_contain_elem (list_of_string str) x 
-							then (true, [x], xs)
-							else (false, [], [])
-			| NClass str -> match line with
+			| Class str -> (match line with
+						[] -> (false, [], [])
+						|x::xs -> if str_contain_elem (list_of_string str) x 
+								then (true, [x], xs)
+								else (false, [], []))
+			| NClass str -> (match line with
 						[] -> (false, [], [])
 						|x::xs -> if str_contain_elem (list_of_string str) x 
 								then (false, [], [])
-								else (true, [x], xs)
+								else (true, [x], xs))
 			| Seq (p,q) -> let (b1, m1, r1) = matchAtStartRE line p in
-					if b1 then let (b2, m2, r2) = matchAtStartRE r q in
-						if b2 then (b2, m1::m2, r2)
+						if b1 then let (b2, m2, r2) = matchAtStartRE r1 q in
+							if b2 then (b2, m1@m2, r2)
+							else (false,[],[])
 						else (false,[],[])
-					else (false,[],[])
 			| Or (p,q) -> let (b1, m1, r1) = matchAtStartRE line p in
 					let (b2, m2, r2) = matchAtStartRE line q in
-						if b1 & b2 
+						if b1 && b2 
 						then 
 							if List.length m1 >= List.length m2 
-								then (b1, m1, r1)
-								else (b2, m2, r2)
+							then (b1, m1, r1)
+							else (b2, m2, r2)
 						else if b1 then (b1, m1, r1)
 						else if b2 then (b2, m2, r1)
 						else (false,[],[])
@@ -100,22 +117,33 @@ let rec matchAtStartRE line re =
 						else (true,[],line)
 			| ZeroOrMore p -> let (b, m, r) = matchAtStartRE line p in
 						if b then matchAtStartRE r p
+						else (true,[],line)
+			| Not p -> let (b, m, r) = matchAtStartRE line p in
+					if b then (false, [], [])
+					else (true, [], line)
+			| OneOrMore p -> let (b, m, r) = matchAtStartRE line p in
+						if b then matchAtStartRE r p
+						else (false, [], [])
+			| Repeat (k, n, p) -> if k > n then (false, [], [])
+						else if n = 0 then (true, [], line)
+						else matchAtStartRep k n line p
+			| _ -> (false, [], [])
+
+
+and matchAtStartRep k n line p = 
+	let (b1, m1, r1) = matchAtStartRE line p in
+			if b1 then
+				if k = 0 then	
+					if n = 0 then (b1, m1, r1)
+					else let (b2, m2, r2) = matchAtStartRep k n-1 r1 p in
+								if b2 then (b2, m1@m2, r2)
+								else (b1, m1, r1)
+				else let (b3,m3, r3) = matchAtStartRep k-1 n-1 r1 p in
+							if b3 then matchAtStartRep k-1 n-1 r3 p
+							else (false, [], [])
+			else (false, [], [])
 ;;
 
-let rec str_contain_elem str_in_list elem =
-		match str_in_list with
-		| [] -> false
-		| x::xs -> if x = elem then true 
-							 else str_contain_elem xs elem
-
-let rec matchAtStartStr line str_in_list =
-	match line, str_in_list with
-		[],_ -> (false, [], [])
-		| _,[] -> (true, [], line)
-		| x::xs,y::ys -> let(b, m, r) = matchAtStartStr xs ys in
-											if x=y && b then (b, y::m, r) 
-											else (false, [], [])
-;;
 
 let matchAtStart line re =
     let (b,m,r) = matchAtStartRE (list_of_string line) re in
